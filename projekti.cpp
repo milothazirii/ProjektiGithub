2a34922285a9960186d151ba84d7bb8e9072fcd2
milothazirii <milothaziri2006@gmail.com>
2025-02-13 23:20:58 +0100

    Create projekti.cpp
    Krijimi i projektit, shtova funksionet bazike.

---
+++ projekti.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+double shuma(int a,int b){
+return a+b;
+}
+double zbritja(int a, int b){
+  return a-b;
+}
+double prodhimi(int a,int b){
+  return a*b;
+}
+double hersi(int a,int b){
+  return a/b;
+}
+
+int main(){
+    double numri1;
+    double numri2;
+    return 0;
+}
